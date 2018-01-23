c546e5fe61ef93b84d1432685dc3b9124e5d2088
Gabriel Stain de Souza <gabriel_sten@hotmail.com>
2018-01-23 07:37:56 -0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PythonCookbook
+
 Learning PythonCookbook
+
+Este repositorio é uma pequena contribuiçāo para a comunidade,
+se destina a programadores mais experientes em python, que visam aprofundar sua
+compreensāo da linguagem e dos modernos recursos da programaçāo.
---
+++ data_structure/tuple.py
@@ -0,0 +1,18 @@
+"""
+Problema
+
+Você tem uma tupla ou sequência de N elementos que gostaria de desempacotar em
+uma coleçāo de N variáveis.
+
+"""
+
+
+"""
+Soluçāo
+
+Qualquer sequência (ou iterável) pode ser desempacotada em variáveis por meio de
+uma operaçāo simples de atribuiçāo. O único requisito é que a quantidade de variáveis
+e a estrutura correspondam à sequência. Por exemplo:
+
+"""
+
