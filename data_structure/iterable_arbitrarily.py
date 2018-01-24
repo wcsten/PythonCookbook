ab803ccc4ddaea90cb85104a6a600a80273190fc
Gabriel Stain de Souza <gabriel_sten@hotmail.com>
2018-01-24 07:59:34 -0200

    learning iterable arbitrarily

--- data_structure/iterable_arbitrarily.py
+++ data_structure/iterable_arbitrarily.py
@@ -22,13 +22,13 @@ desempacotar todas elas, mas e se houver 24? Uma expressāo com asterisco facili
 """
 
 def drop_first_last():
-	"""
-	Funçāo que retorna os termos centrais da lista ignorando o primeiro e o ultimo termo.
-	"""
-	grades = [ 8, 7, 10, 5, 9, 3]
+    """
+    Funçāo que retorna os termos centrais da lista ignorando o primeiro e o ultimo termo.
+    """
+    grades = [ 8, 7, 10, 5, 9, 3]
 
-	first, *middle, last = grades
-	print(middle)
+    first, *middle, last = grades
+    print(middle)
 
 
 """
@@ -40,13 +40,13 @@ podem ser desempacotados desta maneira:
 """
 
 def drop_first_second():
-	"""
-	Funçāo que retorna os termos finais da lista ignorando o primeiro e o segundo termo.
-	"""
-	record = ['Gabriel', 'gabriel@example.com', '99999-9999', '98888-8888']
+    """
+    Funçāo que retorna os termos finais da lista ignorando o primeiro e o segundo termo.
+    """
+    record = ['Gabriel', 'gabriel@example.com', '99999-9999', '98888-8888']
 
-	name, email, *phone_numbers = record
-	print(phone_numbers)
+    name, email, *phone_numbers = record
+    print(phone_numbers)
 
 
 """
@@ -59,20 +59,79 @@ dos primeiros sete trimestres, você pode fazer algo como:
 """
 
 def quarter_comparison():
-	#TODO
-	sales_record = [10, 8, 7, 15, 13, 2, 4, 3]
+    sales_record = [10, 8, 7, 15, 13, 2, 4, 3]
 
-	*trailling_gtrs, current_qtr = sales_record
-	trailling_avg = sum(trailling_gtrs) / len(trailling_gtrs)
+    *trailling_gtrs, current_qtr = sales_record
+    trailling_avg = sum(trailling_gtrs) / len(trailling_gtrs)
+    if trailling_avg > current_qtr:
+        print(trailling_avg)
+    else:
+        print(current_qtr)
+
+
+"""
+
+A sintaxe com asterisco pode ser especialmente útil na iteração por uma sequência de
+tuplas com rótulos:
+
+"""
+
+records = [
+    ('foo', 1, 2),
+    ('bar', 'hello'),
+    ('foo', 4, 5),
+]
+
+def do_foo(x, y):
+    print('foo', x, y)
+
+def do_bar(s):
+    print('bar', s)
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+
+
+"""
+
+O desempacotamento com asterisco também pode ser útil se combinado com determinados
+tipos de operações para processamento de strings, como, por exemplo, uma separação
+(splitting). Por exemplo:
+
+"""
+
+def splitting():
+    line = 'nobody:*:-2:2:Unprivileged User:/var/empty:/usr/bin/false'
+
+    uname, *fields, homedir, sh = line.split(':')
+
+    print(uname, homedir, sh)
 
 
 
+"""
+
+Pode-se imaginar que funções que executem uma separação como essa sejam criadas afim
+de executar algum tipo de algoritimo recursivo inteligente. Por exemplo:
+
+"""
+
+def sums():
+    items = [1, 10, 7, 4, 5, 9]
+    head, *tail = items
+    result = head + sum(tail) if tail else head
+    print(result)
 
 
 if __name__ == '__main__':
-	drop_first_last()
-	drop_first_second()
-
+    drop_first_last()
+    drop_first_second()
+    quarter_comparison()
+    splitting()
+    sums()
 
 
 
